802c62f16fae2b7d03da43b43dcfc995a8550f2d
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-07-19 22:22:20 -0300

    Correção exibir categorias

--- resources/js/Pages/Dashboard/Fatura/Geracoes/Listagem.vue
+++ resources/js/Pages/Dashboard/Fatura/Geracoes/Listagem.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 </script>
 <template>
     <DashboardLayout titulo="Faturas" categoriaPagina="fatura" pagina="faturar">
-        <a href="/dashboard/fatura/download" target="_blank" class="btn btn-success">Exemplo da fatura gerada</a>
+
         <div class="card tabela mt-3 w-100">
             <div class="card-header d-flex justify-content-between align-items-center">
                 <h2 class="card-title">Serviços a faturar</h2>
@@ -32,13 +32,9 @@ const props = defineProps({
                         <tr v-for="servico in servicos.data">
                             <td>{{ servico.collect_date }}</td>
                             <td>{{ servico.pet }}</td>
-                            <td>{{ servico.analises.map(analise => analise.name).join(', ')}}</td>
-                            <td>{{ }}</td>
-                            <td>
-                                <div class="d-flex">
-                                    <Link href="/dashboard/fatura/faturar" class="btn btn-primary ms-2"> Faturar </Link>
-                                </div>
-                            </td>
+                            <td>{{ servico.analises.map(analise => analise.name).join(', ') }}</td>
+                            <td>{{ categorias[servico.cliente.cliente_categoria[0]?.categoria] }}</td>
+                            <td>{{ servico.analises.reduce((acumulador, analise) => acumulador + analise.price, 0) }}</td>
                         </tr>
                         <tr v-if="servicos.data.length === 0">
                             <td colspan="9" class="text-center">Nenhum serviço a faturar</td>
@@ -46,7 +42,7 @@ const props = defineProps({
                     </tbody>
                 </table>
             </div>
-            <Paginacao  :links="servicos.links" />
+            <Paginacao :links="servicos.links" />
         </div>
     </DashboardLayout>
 </template>
