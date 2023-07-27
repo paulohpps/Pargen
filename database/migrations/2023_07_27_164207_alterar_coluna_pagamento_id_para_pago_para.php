4aa94005ee3e79d12d1b2075e86f31ced2b9f6e5
paulohpps <paulohpps13@gmail.com>
2023-07-27 16:55:25 -0300

    Troca no campo "pagamento_id" do lancamentos para "pago_para"

---
+++ database/migrations/2023_07_27_164207_alterar_coluna_pagamento_id_para_pago_para.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('lancamentos', function (Blueprint $table) {
+            $table->dropColumn('pagamento_id');
+            $table->string('pago_para')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('lancamentos', function (Blueprint $table) {
+            $table->unsignedBigInteger('pagamento_id')->nullable();
+        });
+    }
+};
